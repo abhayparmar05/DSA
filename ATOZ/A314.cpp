b751bacbe6d23658b1efc0d9e5e9b1853f23313d
abhayparmar05 <abhayparmar587@gmail.com>
2024-07-17 11:41:40 +0530

    add java collection

--- ATOZ/A314.cpp
+++ ATOZ/A314.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    int arr[10];
-    for (int i = 0; i < arr.; i++)
-    {
-        arr[i] = (int)(Math.random() * 100);
-        cout<<arr[i]<<" ";
-    }
+// int main()
+// {
+//     int arr[10];
+//     for (int i = 0; i < arr.; i++)
+//     {
+//         arr[i] = (int)(Math.random() * 100);
+//         cout<<arr[i]<<" ";
+//     }
 
-    set<int> st;
+//     set<int> st;
 
-    for (int i = 0; i < arr.length; i++)
-    {
-        st.insert(arr[i]);
-    }
-    int index = 0;
-    for (auto it : set)
-    {
-        arr[index] = it;
-        index++;
-    }
-    return 0 ;
-}
+//     for (int i = 0; i < arr.length; i++)
+//     {
+//         st.insert(arr[i]);
+//     }
+//     int index = 0;
+//     for (auto it : set)
+//     {
+//         arr[index] = it;
+//         index++;
+//     }
+//     return 0 ;
+// }
